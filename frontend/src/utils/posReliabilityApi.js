02773d33884e7dee4c037dcc4a28479167f8a880
itsepehr <sepehr.sariaslani@gmail.com>
2026-08-24 14:00:45 +0330

    feat(pos): add background operation API client

--- frontend/src/utils/posReliabilityApi.js
+++ frontend/src/utils/posReliabilityApi.js
@@ -1,7 +1,7 @@
 import { callMethodByPath } from './api'
 
 export function getReliablePOSBoot({ branch = '' } = {}) {
-  return callMethodByPath('restaurant.api_pos_reliability.get_management_pos_boot_reliable', { branch })
+  return callMethodByPath('restaurant.api_pos_background.get_management_pos_boot_safe', { branch })
 }
 
 export function replayOfflinePOSOrder(payload = {}) {
@@ -10,4 +10,23 @@ export function replayOfflinePOSOrder(payload = {}) {
 
 export function updatePOSProductAtomic(payload = {}) {
   return callMethodByPath('restaurant.api_pos_reliability.update_pos_product_atomic', { payload })
+}
+
+export function enqueuePOSBackgroundCheckout(payload = {}, deliver_after = false) {
+  return callMethodByPath('restaurant.api_pos_background.enqueue_pos_checkout', {
+    payload,
+    deliver_after: deliver_after ? 1 : 0,
+  })
+}
+
+export function enqueuePOSBackgroundSettlement(order_name = '', payment = {}, deliver_after = false) {
+  return callMethodByPath('restaurant.api_pos_background.enqueue_pos_settlement', {
+    order_name,
+    payment,
+    deliver_after: deliver_after ? 1 : 0,
+  })
+}
+
+export function getPOSBackgroundOperation(job_key = '') {
+  return callMethodByPath('restaurant.api_pos_background.get_pos_background_operation', { job_key })
 }
